1e2da1682eb78ac1eec3e5135094563a14177fe4
dave <dave@gutcheckit.com>
2017-06-24 19:59:52 -0600

    comments

--- bittrex/bittrex.js
+++ bittrex/bittrex.js
@@ -1,7 +1,17 @@
 const rp = require('request-promise');
 
+/**
+ * parse out the ask (price) from the response object
+ * @param {Object} response bittrex response
+ * @return {Number} price
+ */
 const parse = response => response.result.Ask;
 
+/**
+ * fetch the price info for a single coin pair
+ * @param {String} coin which coin pair to get data for
+ * @return {Promise} price data
+ */
 const fetch = async (coin) => {
   const options = {
     uri: `https://bittrex.com/api/v1.1/public/getticker?market=${coin}`,
@@ -12,6 +22,12 @@ const fetch = async (coin) => {
   return parse(coinData);
 };
 
+/**
+ * Bittrex only supports fetching data for one pair at a time so
+ * kick off those requests in parallel
+ * @param {Object} update containing an exchange and price info
+ * @return {Object} Dictionary of coin symbol to its price
+ */
 const fetchCoinData = async () => {
   const ethP = fetch('BTC-ETH');
   const dashP = fetch('BTC-DASH');
@@ -28,7 +44,10 @@ const fetchCoinData = async () => {
   };
 };
 
-
+/**
+ * Create a reccuring request to bittrex that executes every 5 seconds
+ * @param {EventEmitter} emitter event emitter for signaling updates
+ */
 const connect = (emitter) => {
   console.log('connecting to Bittrex');
 
--- poloniex/poloniex.js
+++ poloniex/poloniex.js
@@ -1,43 +1,44 @@
 const rp = require('request-promise');
 
 const options = {
-    uri: 'https://poloniex.com/public?command=returnTicker',
-    json: true,
+  uri: 'https://poloniex.com/public?command=returnTicker',
+  json: true,
 };
 
-const parsePrice = (resp) => {
-    return Number(resp.lowestAsk);
-};
+const parsePrice = resp => Number(resp.lowestAsk);
 
 const process = (coinData) => {
-    const {BTC_ETH, BTC_DASH, BTC_LTC} = coinData;
-    return { 
-        exchange: 'poloniex',
-        priceInfo: {
-            ETH: parsePrice(BTC_ETH),
-            DASH: parsePrice(BTC_DASH),
-            LTC: parsePrice(BTC_LTC),
-        },
-    };
-
-}
+  const {
+    BTC_ETH,
+    BTC_DASH,
+    BTC_LTC,
+  } = coinData;
+  return {
+    exchange: 'poloniex',
+    priceInfo: {
+      ETH: parsePrice(BTC_ETH),
+      DASH: parsePrice(BTC_DASH),
+      LTC: parsePrice(BTC_LTC),
+    },
+  };
+};
 
 const fetchData = async () => {
-    const coinData = await rp(options);
-    return process(coinData);
-}
+  const coinData = await rp(options);
+  return process(coinData);
+};
 
 
 const connect = (emitter) => {
-    console.log('Connecting to poloniex');
-    
-    setInterval( () => {
-        fetchData()
-            .then( coinData => emitter.emit('update', coinData))
-            .catch( err => console.log('Error connecting to poloniex. Retrying ...'))  
-    }, 5000);
+  console.log('Connecting to poloniex');
+
+  setInterval(() => {
+    fetchData()
+      .then(coinData => emitter.emit('update', coinData))
+      .catch(() => console.log('Error connecting to poloniex. Retrying ...'));
+  }, 5000);
 };
 
 module.exports = {
-    connect,
+  connect,
 };
